7812aa85e44d9a920c8805c5a7d4a89331b78782
Michael Hendrick <mhendrick@byui.edu>
2022-11-28 16:12:25 -0700

    Select statements to calculate employee pay

--- test-cases/select-test.sql
+++ test-cases/select-test.sql
@@ -1,13 +1,16 @@
+SELECT 	SUM(TIMESTAMPDIFF(SECOND, p.clock_in, p.clock_out) / 3600) time_worked
+,		pos.hourly_wage hourly_wage
+,		TRUNCATE(SUM(TIMESTAMPDIFF(SECOND, p.clock_in, p.clock_out) / 3600) * pos.hourly_wage, 2) earnings
+FROM payroll p
+INNER JOIN employees e
+ON p.employee_id = e.employee_id
+INNER JOIN position pos
+ON e.position_id = pos.position_id
+GROUP BY p.employee_id;
 
-SELECT * 
-FROM addresses;
-
-SELECT * 
-FROM franchises;
-
-SELECT * 
-FROM inspections;
-
-SELECT * 
-FROM people;
-
+SELECT TRUNCATE(SUM(TIMESTAMPDIFF(SECOND, p.clock_in, p.clock_out) / 3600) * pos.hourly_wage, 2) total_expenses
+FROM payroll p
+INNER JOIN employees e
+ON p.employee_id = e.employee_id
+INNER JOIN position pos
+ON e.position_id = pos.position_id;
