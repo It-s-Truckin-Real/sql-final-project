506edb6435f6170ef2cb33efa8b76f2c9c3a30f1
Michael Hendrick <mhendrick@byui.edu>
2022-11-28 14:35:15 -0700

    Fixed inspections and drop table statement

--- test-cases/insert-test.sql
+++ test-cases/insert-test.sql
@@ -1,8 +1,14 @@
+-- Drop Temporary Tables
+DROP TEMPORARY TABLE IF EXISTS enumerated_addresses;
+DROP TEMPORARY TABLE IF EXISTS enumerated_franchises;
+DROP TEMPORARY TABLE IF EXISTS enumerated_people;
+
 -- Insert Addresses
 INSERT INTO addresses (street_1, street_2, city, state, zip_code)
 VALUES ("122 N 5th E", null, "Rexburg", "ID", "83440")
 ,      ("320 S 3rd E", "APT 12", "Rexburg", "ID", "83440")
-,      ("456 N 2nd W", "APT 35", "Rexburg", "ID", "83440");
+,      ("456 N 2nd W", "APT 35", "Rexburg", "ID", "83440")
+,      ("456 N 2nd W", "APT 34", "Rexburg", "ID", "83440");
 
 -- Insert People
 CREATE TEMPORARY TABLE IF NOT EXISTS enumerated_addresses (
@@ -46,13 +52,7 @@ VALUES
 INSERT INTO franchises (address_id) 
 (
 	SELECT enumerated_addresses.address_id
-    FROM
-    (
-		SELECT 
-		ROW_NUMBER() OVER(ORDER BY address_id ASC) AS row_num
-		, address_id
-		FROM addresses
-    ) enumerated_addresses
+    FROM enumerated_addresses
     WHERE row_num = 2
 );
 
@@ -61,25 +61,27 @@ INSERT INTO franchises (address_id)
 -- Insert Employees
 
 -- Insert Inspections
+CREATE TEMPORARY TABLE IF NOT EXISTS enumerated_franchises (
+	SELECT 
+	ROW_NUMBER() OVER(ORDER BY franchise_id ASC) AS row_num
+	, franchise_id
+	FROM franchises
+);
+
+CREATE TEMPORARY TABLE IF NOT EXISTS enumerated_people (
+	SELECT 
+	ROW_NUMBER() OVER(ORDER BY person_id ASC) AS row_num
+	, person_id
+	FROM people
+);
+
 INSERT INTO inspections (inspection_date, inspection_score, franchise_id, inspector_person_id)
-VALUES ("2022/11/18", 83, (
+VALUES ("2022-11-18", 83, (
 	SELECT enumerated_franchises.franchise_id
-    FROM
-    (
-		SELECT 
-		ROW_NUMBER() OVER(ORDER BY franchise_id ASC) AS row_num
-		, franchise_id
-		FROM franchises
-    ) enumerated_franchises
-    WHERE row_num = 2), (
+    FROM enumerated_franchises
+    WHERE row_num = 1), (
 	SELECT enumerated_people.person_id
-    FROM
-    (
-		SELECT 
-		ROW_NUMBER() OVER(ORDER BY person_id ASC) AS row_num
-		, person_id
-		FROM people
-    ) enumerated_people
+    FROM enumerated_people
     WHERE row_num = 2));
 
 -- Insert Inspections Employees
@@ -97,6 +99,3 @@ VALUES ("2022/11/18", 83, (
 -- Insert Dish Ingredients
 
 -- Insert Order Dishes
-
--- Drop Temporary Tables
-DROP TEMPORARY TABLE IF EXISTS enumerated_addresses;
