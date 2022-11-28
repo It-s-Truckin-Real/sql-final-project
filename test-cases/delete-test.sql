380b358685e06953e6676d0d07a302c7bbf04320
Michael Hendrick <mhendrick@byui.edu>
2022-11-28 14:46:52 -0700

    Updated delete test

--- test-cases/delete-test.sql
+++ test-cases/delete-test.sql
@@ -1,11 +1,30 @@
+DELETE FROM time_off_requests WHERE request_id >= 1;
+
+DELETE FROM payroll WHERE payroll_id >= 1;
+
+DELETE FROM order_dishes WHERE order_dishes_id >= 1;
+
+DELETE FROM dish_ingredients WHERE dish_ingredients_id >= 1;
+
+DELETE FROM orders WHERE order_id >= 1;
+
+DELETE FROM dishes WHERE dish_id >= 1;
+
+DELETE FROM ingredients WHERE ingredient_id >= 1;
+
+DELETE FROM inspections_employees WHERE inspections_employees_id >= 1;
 
 DELETE FROM inspections WHERE inspection_id >= 1;
 
+DELETE FROM employees WHERE employee_id >= 1;
+
+DELETE FROM position WHERE position_id >= 1;
+
 DELETE FROM franchises WHERE franchise_id >= 1;
+
+DELETE FROM customers WHERE customer_id >= 1;
 
 DELETE FROM people WHERE person_id >= 1;
 
 DELETE FROM addresses WHERE address_id >= 1;
-
-
 
